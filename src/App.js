1e77e6b1021528603bf663ce4ccb01768d423e27
nessy126 <nessy126@gmail.com>
2022-02-18 01:07:50 +0200

    redirect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import reactRouterDom from 'react-router-dom';
+import reactRouterDom, { Redirect } from 'react-router-dom';
 import './App.css';
 import { Route, Switch, NavLink } from "react-router-dom"
 import HomePage from './pages/HomePage';
 import MoviesPage from './pages/MoviesPage';
 import MovieDetailsPage from './pages/MovieDetailsPage';
 import MainMenu from './components/MainMenu/MainMenu';
+import { Suspense } from 'react';
 function App() {
   return (
     <div className="App">
       <MainMenu />
-      <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/movies" exact>
-          <MoviesPage />
-        </Route>
-        <Route path="/movies/:movieId">
-          <MovieDetailsPage />
-        </Route>
-        {/* <Redirect/> */}
-      </Switch>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/movies" exact>
+            <MoviesPage />
+          </Route>
+          <Route path="/movies/:movieId">
+            <MovieDetailsPage />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
     </div>
   )
 }
